42fffb906c24b03ac614ebeb85e1078c046942e3
Eric Singleton <centproram@gmail.com>
2025-04-20 22:57:47 -0700

    udpate home.razor
    udpate home.razor

--- patch-history-app/patch-history-app/obj/Debug/net8.0/patch-history-app.AssemblyInfo.cs
+++ patch-history-app/patch-history-app/obj/Debug/net8.0/patch-history-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ec256d4902a46ea059e0928fcfd7940c5ec1df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c354c8fc586c9cea3613cc2898573039852639")]
 [assembly: System.Reflection.AssemblyProductAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
