7403fc7b9be9876e6c9ba0a6c711a45efdebcf06
Eric Singleton <centproram@gmail.com>
2025-09-29 18:30:04 -0700

    adding patch note feature
    adding patch note feature

---
+++ .github/pull_request_template.md
@@ -0,0 +1,11 @@
+### Summary
+Short description of the change.
+
+### Category (labels recommended)
+- [ ] ui
+- [ ] docs
+- [ ] bugfix
+- [ ] other
+
+### Details (will show in patch notes)
+User-visible impact, notes, links, etc.
--- patch-history-app/patch-history-app/obj/Debug/net8.0/patch-history-app.AssemblyInfo.cs
+++ patch-history-app/patch-history-app/obj/Debug/net8.0/patch-history-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b2ae153be0243f86e3769a1d8d12db1cc185a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8be90a30c2f225847106d57010b6c1e1270a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("patch-history-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
